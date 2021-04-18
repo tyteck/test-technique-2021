c5a71c9efd285670891409099ab78df28cb25455
frederick tyteca <frederick@tyteca.net>
2021-04-18 19:41:06 +0200

    final update

--- start.sh
+++ start.sh
@@ -9,8 +9,12 @@ npm install && npm run prod
 cp .env.example .env
 docker exec -it group-actual php artisan key:generate
 
-echo "merci de permettre à www-data de pouvoir écrire sur bootstrap et storage"
+echo "---------------------------------------------------"
+echo " "
+echo "Merci de permettre à www-data de pouvoir écrire 📝 sur bootstrap et storage"
 echo 'sudo chown -R www-data:$USER storage bootstrap'
 echo 'sudo chmod -R g+rw storage bootstrap'
+echo " "
+echo "---------------------------------------------------"
 
 echo "une fois que tout ca est ok vous pouvez ouvrir votre navigateur sur http://localhost:8088"
