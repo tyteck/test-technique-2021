9efea8c422c2670208974e6ea0828643be49d767
frederick tyteca <frederick@tyteca.net>
2021-04-18 21:09:43 +0200

    adding the command line to type to run fibonacci

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,12 @@
 Exercices à réaliser :
 <ul>
   <li>ACT-TT1 :* Réaliser une page web qui <a href="{{ route('chess') }}" class="underline">affichera un échiquier</a>. </li>
-  <li>ACT-TT2 :* Réaliser une commande qui prend en argument un chiffre n et retourne un tableau contenant une suite de Fibonacci de la longueur n. ***Tips** : Ne vous limittez pas à un copié / collé !</li>
+  <li>
+    ACT-TT2 :* Réaliser une commande qui prend en argument un chiffre n et retourne un tableau contenant une suite de Fibonacci de la longueur n. ***Tips** : Ne vous limittez pas à un copié / collé !
+    <ul class="list-disc pl-8">
+      <li><pre>docker exec -it group-actual php artisan fibonacci &lt;number&gt; </pre></li>
+    </ul>
+  </li>
   <li>ACT-TT3 :* Réaliser une modélisation d'un candidat, d'une annonce et d'une candidature en utilisant l'orm Eloquent et créer un service qui <a href="{{ route('annonces.index') }}" class="underline">liste toutes les candidatures par annonce</a>.</li>
   <li>ACT-TT4 :* Réaliser une suite de test unitaire pour les 3 premiers exercices.</li>
 </ul>
